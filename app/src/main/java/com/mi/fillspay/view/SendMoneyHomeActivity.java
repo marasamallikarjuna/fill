1bf6b7fc5d85e40d139d9e45bb4e9f43a546f47c
marasamallikarjuna <marasamallikarjuna@gamil.com>
2020-01-28 16:57:51 +0530

    added fab menu in send money screen

--- app/src/main/java/com/mi/fillspay/view/SendMoneyHomeActivity.java
+++ app/src/main/java/com/mi/fillspay/view/SendMoneyHomeActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,23 +14,32 @@ import com.mi.fillspay.R;
 import com.mi.fillspay.adapter.CircleLayoutManager;
 import com.mi.fillspay.adapter.SampleAdapter;
 import com.mi.fillspay.utilities.FragmentUtil;
+import com.mi.fillspay.utilities.speeddial.SpeedDialActionItem;
+import com.mi.fillspay.utilities.speeddial.SpeedDialView;
 
 
 public class SendMoneyHomeActivity extends AppCompatActivity {
 
-    ViewGroup root;
-    RecyclerView list;
-    LinearLayout circularText;
-    private CircleLayoutManager layoutManager;
-    private SampleAdapter adapter;
-    TextView textViewSide;
+    SpeedDialView speedDialView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_money_main);
 
+        initValues();
+
         FragmentUtil.setFragment(new SendMoneyFragOne(),this,"Send Mobile fragment one", R.id.content_frag,false);
+
+    }
+
+    private void initValues() {
+
+        speedDialView = findViewById(R.id.speedDial_id);
+
+        speedDialView.setBackground(getResources().getDrawable(R.drawable.fab_open_bg));
+
+        initSpeedDial();
 
         findViewById(R.id.icon_reg).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,4 +50,31 @@ public class SendMoneyHomeActivity extends AppCompatActivity {
 
     }
 
+    private void initSpeedDial() {
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_item2, R.drawable.ic_fabmenu_item3).create());
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_item1, R.drawable.ic_fabmenu_item1).create());
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.fab_item3, R.drawable.ic_fabmenu_item3).create());
+
+        speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @Override
+            public boolean onActionSelected(SpeedDialActionItem actionItem) {
+                switch (actionItem.getId()){
+                    case R.id.fab_item1:
+                        Toast.makeText(SendMoneyHomeActivity.this, "one", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.fab_item2:
+                        Toast.makeText(SendMoneyHomeActivity.this, "two", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.fab_item3:
+                        Toast.makeText(SendMoneyHomeActivity.this, "three", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
 }
