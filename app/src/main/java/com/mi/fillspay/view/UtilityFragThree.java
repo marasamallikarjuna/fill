f9e4531b92f90fc611b202464776889ff79ebe5f
marasamallikarjuna <marasamallikarjuna@gamil.com>
2020-02-06 12:10:42 +0530

    added dynamic edittext to biller descr

--- app/src/main/java/com/mi/fillspay/view/UtilityFragThree.java
+++ app/src/main/java/com/mi/fillspay/view/UtilityFragThree.java
@@ -1,18 +1,21 @@
 package com.mi.fillspay.view;
 
-
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.mi.fillspay.R;
@@ -21,10 +24,13 @@ import com.mi.fillspay.local.prefe.AppPreferencesHelper;
 import com.mi.fillspay.model.BillerDescResponse;
 import com.mi.fillspay.model.ConsumerNoFormatRequest;
 import com.mi.fillspay.model.ConsumerNoFormatResponse;
+import com.mi.fillspay.model.ListOfIOCatalog;
+
+import android.widget.LinearLayout.LayoutParams;
+
 import com.mi.fillspay.utilities.FragmentUtil;
 import com.mi.fillspay.utilities.GradientTextView;
 import com.mi.fillspay.utilities.SwipeButton;
-import com.mi.fillspay.view_model.BillerDescViewModel;
 import com.mi.fillspay.view_model.ConsumerNoFormatViewModel;
 
 import static com.mi.fillspay.interfaces.keys.ACTIVITY_RESULT;
@@ -39,8 +45,10 @@ public class UtilityFragThree extends Fragment {
     GradientTextView headerName;
     SwipeButton swipeButton;
     TextView tv_boardName;
-    AppCompatEditText actv;
+    AppCompatEditText actv, et_bill_amount;
     ConsumerNoFormatViewModel consumerViewModel;
+    LinearLayout consumerLayout;
+
 
     public UtilityFragThree() {
         // Required empty public constructor
@@ -64,6 +72,8 @@ public class UtilityFragThree extends Fragment {
         tv_boardName = getActivity().findViewById(R.id.board_name);
         swipeButton = getActivity().findViewById(R.id.swipe_btn_id);
         actv = getActivity().findViewById(R.id.autoComBillers_ids);
+        consumerLayout = getActivity().findViewById(R.id.layout_cnum_format);
+        et_bill_amount = getActivity().findViewById(R.id.amount_et_billerdes);
 
         if (getArguments() != null) {
             headerName.setText("Pay Your " + getArguments().getString(UTILITY_NAME) + " Bill");
@@ -84,7 +94,7 @@ public class UtilityFragThree extends Fragment {
             @Override
             public void onSwipeListener() {
                 Fragment frag = new PaymentFrag();
-                FragmentUtil.setFragment(frag, getActivity(), "Utility fragment one",R.id.content_frag, true);
+                FragmentUtil.setFragment(frag, getActivity(), "Utility fragment one", R.id.content_frag, true);
             }
         });
     }
@@ -97,6 +107,11 @@ public class UtilityFragThree extends Fragment {
             if (data != null) {
                 BillerDescResponse item = data.getParcelableExtra("MESSAGE");
                 actv.setText(item.getBillerName());
+
+                consumerLayout.removeAllViewsInLayout();
+
+                consumerLayout.removeAllViews();
+
                 getConsumerNumberFormat(item.getBillerID());
             }
         }
@@ -104,16 +119,46 @@ public class UtilityFragThree extends Fragment {
 
     private void getConsumerNumberFormat(String biller_id) {
 
-        _preferencesHelper = new AppPreferencesHelper(getActivity(),"Spandana");
+        _preferencesHelper = new AppPreferencesHelper(getActivity(), "Spandana");
 
         consumerViewModel = ViewModelProviders.of(getActivity()).get(ConsumerNoFormatViewModel.class);
 
-       /* consumerViewModel.getConsmerNoFarmat(new ConsumerNoFormatRequest("1","1",biller_id),_preferencesHelper.getAccessToken()).observe(new Observer<ConsumerNoFormatResponse>() {
+        consumerViewModel.getConsmerNoFarmat(new ConsumerNoFormatRequest("1", "1", biller_id), _preferencesHelper.getAccessToken(),getActivity()).observe(this, new Observer<ConsumerNoFormatResponse>() {
             @Override
             public void onChanged(ConsumerNoFormatResponse consumerNoFormatResponse) {
 
+                for (int i = 0; i < consumerNoFormatResponse.getListOfIOCatalog().size(); i++) {
+                    onAddField(consumerNoFormatResponse.getListOfIOCatalog().get(i));
+                }
             }
-        });*/
+        });
+    }
 
+    public void onAddField(ListOfIOCatalog itemConstrains) {
+
+        // add text view
+        final TextView tv = new TextView(getActivity());
+        tv.setText(itemConstrains.getName().trim());
+        tv.setPadding(3, 6, 3, 3);
+        tv.setTextColor(getResources().getColor(R.color.greyTextcolor));
+        tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        consumerLayout.addView(tv);
+
+        // add edit text
+        final EditText et_ = new EditText(getActivity());
+        et_.setTextSize(R.dimen._13ssp);
+        int img = R.drawable.ic_country_icon;
+        et_.setCompoundDrawablesWithIntrinsicBounds(img, 0, 0, 0);
+        et_.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        et_.getBackground().setColorFilter(getResources().getColor(R.color.colorEdit),
+                PorterDuff.Mode.SRC_ATOP);
+        setEditTextMaxLength(et_, itemConstrains.getMaxLength());
+        consumerLayout.addView(et_);
+    }
+
+    public static void setEditTextMaxLength(EditText editText, int length) {
+        InputFilter[] FilterArray = new InputFilter[1];
+        FilterArray[0] = new InputFilter.LengthFilter(length);
+        editText.setFilters(FilterArray);
     }
 }
